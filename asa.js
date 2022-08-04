5153e4fa30105e4eab150ecd01dcfee45d67753a
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2022-08-04 19:39:39 +0600

    content added

---
+++ asa.js
@@ -0,0 +1,44 @@
+else if(jane >= 40 && jane <= 49){
+    console.log('Jane got D Grade');
+}
+else if(jane >= 50 && jane <= 59){
+    console.log('Jane got C Grade');
+}
+else if(jane >= 60 && jane <= 79){
+    console.log('Jane got B Grade');
+}
+else{
+    console.log('Great Jane, You Got A+');
+}
+// Peter
+if(peter >= 0 && peter <= 39){
+    console.log('Peter got F Grade');
+}
+else if(peter >= 40 && peter <= 49){
+    console.log('Peter got D Grade');
+}
+else if(peter >= 50 && peter <= 59){
+    console.log('Peter got C Grade');
+}
+else if(peter >= 60 && peter <= 79){
+    console.log('Peter got B Grade');
+}
+else{
+    console.log('Great Peter, You Got A+');
+}
+// john
+if(john >= 0 && john <= 39){
+    console.log('John got F Grade');
+}
+else if(john >= 40 && john <= 49){
+    console.log('John got D Grade');
+}
+else if(john >= 50 && john <= 59){
+    console.log('John got C Grade');
+}
+else if(john >= 60 && john <= 79){
+    console.log('John got B Grade');
+}
+else{
+    console.log('Great John, You Got A+');
+}
